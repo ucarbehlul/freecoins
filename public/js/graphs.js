c23dbd1faeba7dfd8948fc723b5a6a9c67a5b126
Mark Szymanski <mdszy@me.com>
2013-03-10 19:52:00 -0500

    more comments!

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,9 +1,13 @@
 var drawGraphs = function(data) {
+  // Now that we have our data and are drawing the graph, we can stop the spinner.
   $('#payout-graph').spin(false);
 
-  console.log(data)
-
-  var rawData = []
+  var rawData = [];
+  // The raw data that the graphing library uses is
+  // in the form of [ [x, y], [x, y], ... ] so we generate
+  // that data from the data downloaded from the server. The
+  // timestamp that the graphing library requires is in UNIX
+  // timestamp form. That is, milliseconds since Jan 1, 1970.
 
   for(var i = 0; i < data.payouts.length; i++) {
     var payout = data.payouts[i];
@@ -11,11 +15,13 @@ var drawGraphs = function(data) {
     rawData.push([payout.timestamp, payout.transactions * data.payout_amount])
   }
 
+  // This is the data object our graphing library uses.
   data = {
     data: rawData,
     label: "Payouts"
   }
 
+  // And some basic options that control how the graph is displayed.
   var options = {
     series: {
       lines: {
@@ -47,8 +53,10 @@ var drawGraphs = function(data) {
       show: true
     }
   }
+  // Now we plot it!
   $("#payout-graph").plot([data], options);
 
+  // This draws the y-axis label after the graph has been drawn.
   var yaxisLabel = $("<div class='axisLabel yaxisLabel'></div>")
     .text("Payout Amount (BTC)")
     .appendTo($("#graph-container"));
@@ -60,8 +68,14 @@ var drawGraphs = function(data) {
   yaxisLabel.css("margin-top", yaxisLabel.width() / 2 - 20);
 }
 
+// This function is called right when the page for the graphs
+// is loaded.
 $(function($) {
+  // First we start a spinner in the graph while we get the data.
   $('#payout-graph').spin();
+
+  // Now we get the data used to draw the graphs and call our
+  // drawGraphs function once we're done.
   $.ajax({
     url: '/data',
     type: 'get',
